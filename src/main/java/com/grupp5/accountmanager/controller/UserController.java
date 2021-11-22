b1d49dc2444ab5f69b372a98e232ff317a18bc87
Alexander Frödin <alexanderdelaval@gmail.com>
2021-11-22 16:22:09 +0100

    fixed annotation value

--- src/main/java/com/grupp5/accountmanager/controller/UserController.java
+++ src/main/java/com/grupp5/accountmanager/controller/UserController.java
@@ -3,6 +3,7 @@ package com.grupp5.accountmanager.controller;
 import com.grupp5.accountmanager.models.UserM;
 import com.grupp5.accountmanager.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
-   // @Value("${App.Url}")
-   // private String base_url;
+    @Value("${app.base.url}")
+    private String base_url;
 
     @Autowired
-    UserServiceImpl userService;
-
+    private UserServiceImpl userService;
 
     @GetMapping("/api/user/{id}")
     public ResponseEntity<UserM> findById(@PathVariable Long id) {
@@ -25,7 +25,7 @@ public class UserController {
 
     @PostMapping("/api/user/create")
     public ResponseEntity<UserM> createUser(@RequestBody UserM user) {
-        System.out.println("test");
+        System.out.println(base_url);
         UserM createdUser = userService.createUser(user.getUserName(), user.getUserEmail(), user.getUserImageUrl(), user.getHashedPassword()); // TODO: Fix separate "password" and "hashedPassword"
 
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
